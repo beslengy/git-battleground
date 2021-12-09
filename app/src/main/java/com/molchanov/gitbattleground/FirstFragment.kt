556448887767deceb354cd91a5c5dd65ec6269ee
Yaroslav Molchanov <jaroslav.m@chillicode.ru>
2021-12-09 18:17:33 +0300

    master 1

--- app/src/main/java/com/molchanov/gitbattleground/FirstFragment.kt
+++ app/src/main/java/com/molchanov/gitbattleground/FirstFragment.kt
@@ -37,12 +37,7 @@ class FirstFragment : Fragment() {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
-    // master 1
-    // master 2
-    // master 3
-    //1
-    //2
-
+    //master 1
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
